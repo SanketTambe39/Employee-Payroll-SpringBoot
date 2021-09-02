f69948f98b536e3698b6ab0f33775f97bdd95977
SanketTambe39 <sankettambe39@gmail.com>
2021-09-03 00:29:52 +0530

    ADDED: Logs files for production and development

--- src/main/java/com/bridgelabz/employeepayrollspringboot/EmployeePayrollSpringBootApplication.java
+++ src/main/java/com/bridgelabz/employeepayrollspringboot/EmployeePayrollSpringBootApplication.java
@@ -3,14 +3,15 @@ package com.bridgelabz.employeepayrollspringboot;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollSpringBootApplication.class, args);
-		log.info("Application Started Successfully...!!");
+		ApplicationContext context =SpringApplication.run(EmployeePayrollSpringBootApplication.class, args);
+		log.info("Application Started Successfully. in {} Enviroment",context.getEnvironment().getProperty("environment"));
 	}
 
 }
